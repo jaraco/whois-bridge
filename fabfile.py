cd912c29323a18eac4a0b4653685864007a3413e
Jason R. Coombs <jaraco@jaraco.com>
2016-08-07 13:52:42 -0400

    Update config for Xenial and punisher.

--- fabfile.py
+++ fabfile.py
@@ -1,14 +1,13 @@
 from fabric.api import sudo, run, task, env
 from fabric.contrib import files
-from jaraco.fabric import apt
-from jaraco.fabric import context
-from jaraco.util.string import (
+from fabric.context_managers import shell_env
+from jaraco.text import (
 	local_format as lf,
 	global_format as gf,
 )
 
 if not env.hosts:
-	env.hosts = ['elektra']
+	env.hosts = ['punisher']
 
 install_root = '/opt/whois-bridge'
 proc_name = 'whois-bridge'
@@ -26,14 +25,14 @@ def install_env():
 @task
 def install_upstart_conf(install_root=install_root):
 	sudo(lf('mkdir -p "{install_root}"'))
-	conf = gf("ubuntu/{proc_name}.conf")
-	files.upload_template(conf, "/etc/init",
+	conf = gf("ubuntu/{proc_name}.service")
+	files.upload_template(conf, "/etc/systemd/system",
 		use_sudo=True, context=vars())
 
 @task
 def update(version=None):
 	install_to(install_root, version, use_sudo=True)
-	cmd = gf('restart {proc_name} || start {proc_name}')
+	cmd = gf('systemctl restart {proc_name}')
 	sudo(cmd)
 
 def install_to(root, version=None, use_sudo=False):
@@ -45,21 +44,22 @@ def install_to(root, version=None, use_sudo=False):
 	pkg_spec = 'jaraco.net'
 	if version:
 		pkg_spec += '==' + version
-	action(lf('mkdir -p "{root}/lib/python3.4/site-packages"'))
-	with apt.package_context('python-dev'):
-		with context.shell_env(PYTHONUSERBASE=root):
+	if True: #with apt.package_context('python-dev'):
+		with shell_env(PYTHONUSERBASE=root):
+			usp = run('python3 -c "import site; print(site.getusersitepackages())"')
+			action('mkdir -p {usp}'.format(**locals()))
+			# can't run with '-U' because that will cause lxml to upgrade/build
 			cmd = [
-				'python3.4', '-m',
-				'easy_install',
+				'python3',
+				'-m', 'pip',
+				'install',
 				'--user',
-				'-U',
-				#'-f', 'http://dl.dropbox.com/u/54081/cheeseshop/index.html',
 				pkg_spec,
 			]
 			action(' '.join(cmd))
 
 @task
 def remove_all():
-	sudo(gf('stop {proc_name} || echo -n'))
-	sudo(gf('rm /etc/init/{proc_name}.conf || echo -n'))
+	sudo(gf('systemctl stop {proc_name} || echo -n'))
+	sudo(gf('rm /etc/systemd/system/{proc_name}.service || echo -n'))
 	sudo(gf('rm -Rf "{install_root}"'))
